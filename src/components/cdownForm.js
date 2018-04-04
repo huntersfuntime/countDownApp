1c61006a33b8288d41760d37311bcba28e627f89
huntersfuntime <hunterorges@gmail.com>
2018-04-04 16:47:42 -0600

    Component for cdown form handling change

--- src/components/cdownForm.js
+++ src/components/cdownForm.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
+import moment from 'moment';
 
 class CdownForm extends Component {
 
     constructor(props) {
         super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+            startDate: moment()
+        }
     }
 
+    handleChange(date) {
+
+        alert(date);
+    }
 
     render() {
         return (
             <div>
                 <h1> WINTER IS COMING </h1>
-                <DatePicker />
+                <DatePicker 
+                    selected={this.state.startDate}
+                    onChange={this.handleChange}
+                />
             </div>
         )
     }
